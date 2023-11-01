3646e018fb3c9e37fb320c753f5474e8c778c475
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-11-01 23:52:30 +0000

    update schema with votes table

--- cmd/yap-migrate/1-init.up.sql
+++ cmd/yap-migrate/1-init.up.sql
@@ -1,13 +1,22 @@
 CREATE TABLE yaps (
     /* xid formatted */
     'id' TEXT,
-    /* min 1 character, max 240 characters */
     'content' TEXT NOT NULL,
-    /* TODO add region */
     'region' TEXT NOT NULL DEFAULT 'lhr'
     --
-    CONSTRAINT yap_content CHECK (content <> '' AND length(content) <= 240),
+    CONSTRAINT valid_content CHECK (content <> '' AND length(content) <= 240),
+    CONSTRAINT fly_region CHECK (region IN ('lhr', 'syd', 'iad')),
     PRIMARY KEY (id)
 );
 
 /* yaps upvotes, downvotes, json events */
+CREATE TABLE votes (
+    /* https://stackoverflow.com/questions/7905859/is-there-auto-increment-in-sqlite */
+    'id' INT,
+    'yap' TEXT NOT NULL,
+    'score' INT NOT NULL DEFAULT 1,
+    --
+    CONSTRAINT is_score CHECK (score IN (0, 1)),
+    FOREIGN KEY (yap) REFERENCES yaps (id),
+    PRIMARY KEY (id)
+);
